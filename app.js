264a912f343274bee2149bacb9930ea955346420
Kadu Nascimento <163493195+Kaduzin008@users.noreply.github.com>
2025-11-28 18:10:08 -0300

    Alterando nome de Views para views

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 app.set("view engine", "ejs");
-app.set("views", "./Views");
+app.set("views", "./views");
 //app.use(express.static('./public'))
 
 
