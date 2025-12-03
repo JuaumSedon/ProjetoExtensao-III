c0703639771287b51506457f37523d6e65805bf3
JuaumSedon <jvg020804@gmail.com>
2025-12-03 19:37:46 -0300

    mudando cache do  BD

--- Config/dbConn.js
+++ Config/dbConn.js
@@ -1,23 +1,35 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 
-dotenv.config();
 
-const conexaoBD = async() =>{
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
+let isConnected = false; // Cache da conexão // Problema do vercel
+
+const conexaoBD = async () => {
+    
+    if (isConnected) {
+        return;
+    }
+    if (!process.env.MONGO_URI) {
+        console.error("❌ ERRO FATAL: A variável MONGO_URI não foi encontrada.");
+        console.error("Verifique se o arquivo .env existe e se o nome está correto.");
+        return;
+    }
 
     try {
+        const db = await mongoose.connect(process.env.MONGO_URI, {
+            serverSelectionTimeoutMS: 5000
+        });
 
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Conectado ao banco");
+        isConnected = db.connections[0].readyState;
+        console.log("✅ Conectado ao MongoDB com sucesso!");
         
     } catch (error) {
-
-        console.log("Erro ao conectar ao banco de dados ", error);
-        
+        console.error("❌ Erro ao conectar no MongoDB:", error);
+        throw error;
     }
 }
 
-
 module.exports = conexaoBD;
-
