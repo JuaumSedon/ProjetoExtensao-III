c68167ab460a20b1a11c962425cd5c225022d433
JuaumSedon <jvg020804@gmail.com>
2025-12-03 16:23:38 -0300

    mudando css

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
